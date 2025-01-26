0cc2c5f8e11c27cd6884e0fddb8f689357f622f3
mrczarny <j.adamski@zsebydgoszcz.onmicrosoft.com>
2025-01-26 16:44:32 +0100

    init

---
+++ app.py
@@ -0,0 +1,25 @@
+﻿from flask import Flask
+from dataModel import db, Data
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/data', methods=['POST'])
+def add_data():
+    db.session.add(Data(timestamp='2020-01-01 00:00:00', temperature=25.0, humidity=50.0))
+    db.session.commit()
+    return 'Data added'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ dataModel.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+class Data(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    timestamp: Mapped[str] = mapped_column(nullable=False)
+    temperature: Mapped[float] = mapped_column(nullable=False)
+    humidity: Mapped[float] = mapped_column(nullable=False)
+    
+    
+    
+    # id = db.Column(db.Integer, primary_key=True)
+    # timestamp = db.Column(db.DateTime, nullable=False)
+    # temperature = db.Column(db.Float, nullable=False)
+    # humidity = db.Column(db.Float, nullable=False)
+
+    # def __init__(self, timestamp, temperature, humidity):
+    #     self.timestamp = timestamp
+    #     self.temperature = temperature
+    #     self.humidity = humidity
+
